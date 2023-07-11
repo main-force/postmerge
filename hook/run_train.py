3df544ad2f4e198c58879a6c4c92c2384dbb277a
김주력 <ryan9423@hanyang.ac.kr>
2023-07-11 16:27:25 +0900

    add requirements.txt

--- hook/run_train.py
+++ hook/run_train.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from keras import layers
